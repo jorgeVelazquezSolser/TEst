51f44ee81c59f5aaa808de7c0bb03fc7d21ea14f
JorgeVelazquez <jorge.velazquez@solsersistem.net>
2025-02-20 16:17:25 -0600

    Se sube nueco controla y se vera publicacion con jenkins

---
+++ WebApplication1/WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GET() 
+        { 
+            return Ok("ES un ok");
+        
+        }
+    }
+}
